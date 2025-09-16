8674e036921b89649d68f126632caec0ce9d0510
roby <roberto.praolini@gmail.com>
2025-09-16 19:03:43 +0200

    Aggiornati i controller

--- src/main/java/gestione/controller/ServizioController.java
+++ src/main/java/gestione/controller/ServizioController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/servizi")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ServizioController {
 
     @Autowired
--- src/main/java/gestione/controller/TipoServerController.java
+++ src/main/java/gestione/controller/TipoServerController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tipi-server")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TipoServerController {
 
     @Autowired
